dfc12f8ab80f01d77701337cae71583d6aebb076
Oscar-Sch <oscarschwerdt@gmail.com>
2023-03-22 15:01:45 -0300

    project setup

---
+++ src/test/java/com/mindhub/merchshop/MerchshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindhub.merchshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerchshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
