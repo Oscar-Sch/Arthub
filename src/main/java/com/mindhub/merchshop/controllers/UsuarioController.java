55b943a443fe429d4b6bffdbffb7b9c50c565ae2
CameronVelasquez <cameronlino@outlook.com>
2023-03-24 00:52:51 -0300

    Controladores added

---
+++ src/main/java/com/mindhub/merchshop/controllers/UsuarioController.java
@@ -0,0 +1,17 @@
+package com.mindhub.merchshop.controllers;
+
+import com.mindhub.merchshop.models.Usuario;
+import com.mindhub.merchshop.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+
+@RestController
+public class UsuarioController {
+
+
+}
