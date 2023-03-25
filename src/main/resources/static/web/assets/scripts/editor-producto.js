a401cb6b5944c25ae5f6d2cab38b2d9350f2778c
JoelAlexanderTrinidad <joelalexandertrinidad@gmail.com>
2023-03-25 14:48:21 -0300

    carrousel editor done

--- src/main/resources/static/web/assets/scripts/editor-producto.js
+++ src/main/resources/static/web/assets/scripts/editor-producto.js
@@ -3,7 +3,12 @@ const { createApp } = Vue;
 createApp({
     data(){
         return {
-           
+            currentIndex: 0,
+            images: [
+                'https://picsum.photos/id/237/500/300',
+                'https://picsum.photos/id/238/500/300',
+                'https://picsum.photos/id/239/500/300'
+              ]
         }
     },
     created(){
@@ -19,6 +24,19 @@ createApp({
                 container.style.width = "11rem";
             }
         },
+        prevImage() {
+            console.log("prev")
+            this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length
+          },
+          nextImage() {
+            console.log("next")
+            this.currentIndex = (this.currentIndex + 1) % this.images.length
+          }
+    },
+    computed: {
+        currentImage() {
+          return this.images[this.currentIndex]
+        }
     }
   
 }).mount("#app")
