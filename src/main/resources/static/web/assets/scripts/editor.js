caed35c522c3864edd33d2d452e9d7e1ec635e63
JoelAlexanderTrinidad <joelalexandertrinidad@gmail.com>
2023-03-28 13:01:38 -0300

    movil view editro in progress

---
+++ src/main/resources/static/web/assets/scripts/editor.js
@@ -0,0 +1,60 @@
+const { createApp } = Vue;
+
+createApp({
+    data(){
+        return {
+            currentIndex: 0,
+            images: [
+                '../web/assets/images/illustrations/illust_Joel/1.png',
+                '../web/assets/images/illustrations/illust_Joel/2.png',
+                '../web/assets/images/illustrations/illust_Joel/3.png'
+            ],
+            seleccionado: 'vista-previa__editor-producto',
+            colorSeleccionado: '',
+        }
+    },
+    created(){
+       
+
+    },
+    methods:{
+        openMenu() {
+            let container=document.querySelector(".menu-container");
+            if (container.style.width=="11rem"){
+                container.style.width = "4.2rem";
+            }else{
+                container.style.width = "11rem";
+            }
+        },
+        prevImage() {
+            console.log("prev")
+            this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length
+          },
+        nextImage() {
+            console.log("next")
+            this.currentIndex = (this.currentIndex + 1) % this.images.length
+        },
+        llavero(){
+
+        },
+        cambiarColor(color){
+            console.log(color)
+            this.colorSeleccionado = color
+
+
+        },
+        seleccion(icono){
+            this.seleccionado = icono
+            console.log(icono)
+            if(icono == 'vista-previa__editor-producto-llavero' || icono == 'vista-previa__editor-producto-cuaderno'  || icono == 'vista-previa__editor-producto-retrato' || icono == 'vista-previa__editor-producto-taza'){
+                this.colorSeleccionado = ''
+            }
+        }
+    },
+    computed: {
+        currentImage() {
+          return this.images[this.currentIndex]
+        }
+    }
+  
+}).mount("#app")
